c06d0ca4617721ac72c65da82f6a01457d8066b0
Craig Dumont <cldumont77@comcast.net>
2019-03-10 18:08:21 -0400

    Added new TableViewController for all the maintenance.

---
+++ HomeMechanic/View Controllers/AllMaintenanceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AllMaintenanceViewController.swift
+//  HomeMechanic
+//
+//  Created by Craig Dumont on 3/10/19.
+//  Copyright © 2019 Craig Dumont. All rights reserved.
+//
+
+import UIKit
+
+class AllMaintenanceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
