804dc325635fd62547c217d764add9aebac6c5ff
Craig Dumont <cldumont77@comcast.net>
2019-03-18 23:45:11 -0400

    View is sliding above keyboard.

--- HomeMechanic/View Controllers/AddEditItemViewController.swift
+++ HomeMechanic/View Controllers/AddEditItemViewController.swift
@@ -48,7 +48,41 @@ class AddEditItemViewController: UIViewController {
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(AddEditItemViewController.viewTapped(gestureRecognizer:)))
         view.addGestureRecognizer(tapGesture)
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardDidShow(notification: Notification) {
+        let info: NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let keyboardY = self.view.frame.height - keyboardSize.height
+        
+        let editingTextViewY: CGFloat! = self.notesTextview?.frame.origin.y
+        
+        //Checking if the textView is really hidden behind the keyboard
+        if self.view.frame.origin.y >= 0 {
+            if editingTextViewY > keyboardY - 200 {
+                UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+                    self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y - (editingTextViewY! - (keyboardY - 200)), width: self.view.bounds.width, height: self.view.bounds.height)
+                }, completion: nil)
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+            self.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+        }, completion: nil)
+    }
+    
     
     @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
         view.endEditing(true)
@@ -124,6 +158,10 @@ extension AddEditItemViewController: UITextFieldDelegate, UITextViewDelegate {
         let newText = oldText.replacingCharacters(in: stringRange, with: text)
         doneBarButton.isEnabled = !newText.isEmpty
         return true
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        notesTextview = textView
     }
 
 }
