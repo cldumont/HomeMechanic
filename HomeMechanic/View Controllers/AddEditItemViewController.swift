9aaeac44784dfa27127f0590f0d492f7d7a48ab2
Craig Dumont <cldumont77@comcast.net>
2019-03-20 23:34:18 -0400

    More UI changes. Having issues moving the view with the keyboard.

--- HomeMechanic/View Controllers/AddEditItemViewController.swift
+++ HomeMechanic/View Controllers/AddEditItemViewController.swift
@@ -21,6 +21,7 @@ class AddEditItemViewController: UIViewController {
     weak var itemToEdit: MaintenanceItem?
     private var datePicker: UIDatePicker?
     
+    @IBOutlet weak var addEditLabel: UILabel!
     @IBOutlet weak var dateTextfield: UITextField!
     @IBOutlet weak var odometerTextfield: UITextField!
     @IBOutlet weak var notesTextview: UITextView!
@@ -34,25 +35,24 @@ class AddEditItemViewController: UIViewController {
         odometerTextfield.delegate = self
         notesTextview.delegate = self
         
-        dateTextfield.becomeFirstResponder()
         addDatePicker()
         
         if let item = itemToEdit {
-            title = "Edit Item"
+            addEditLabel.text = "Edit Maintenance Item"
             dateTextfield.text = item.date
             odometerTextfield.text = item.odometer
             notesTextview.text = item.notes
             doneBarButton.isEnabled = true
         } else {
-            title = "Add Item"
+            addEditLabel.text = "Add Maintenance Item"
         }
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(AddEditItemViewController.viewTapped(gestureRecognizer:)))
         view.addGestureRecognizer(tapGesture)
         
-        let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+//        let notificationCenter = NotificationCenter.default
+//        notificationCenter.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+//        notificationCenter.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
         
         
         let numberToolbar: UIToolbar = UIToolbar()
@@ -62,14 +62,15 @@ class AddEditItemViewController: UIViewController {
     
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        dateTextfield.becomeFirstResponder()
+    }
+    
     @objc func viewTapped(gestureRecognizer: UITapGestureRecognizer) {
         view.endEditing(true)
     }
     
-    @objc func doneNumPad () {
-        // Take a look bounces on smaller screens
-        odometerTextfield.resignFirstResponder()
-        
+    @objc func doneNumPad() {
         notesTextview.becomeFirstResponder()
     }
     
@@ -78,28 +79,28 @@ class AddEditItemViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    @objc func keyboardDidShow(notification: Notification) {
-        let info: NSDictionary = notification.userInfo! as NSDictionary
-        let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
-        let keyboardY = self.view.frame.height - keyboardSize.height
-
-        let editingTextViewY: CGFloat! = self.notesTextview?.frame.origin.y
-
-        //Checking if the textView is really hidden behind the keyboard
-        if self.view.frame.origin.y >= 0 {
-            if editingTextViewY > keyboardY - 200 {
-                UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
-                    self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y - (editingTextViewY! - (keyboardY - 200)), width: self.view.bounds.width, height: self.view.bounds.height)
-                }, completion: nil)
-            }
-        }
-    }
-
-    @objc func keyboardWillHide(notification: Notification) {
-        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
-            self.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
-        }, completion: nil)
-    }
+//    @objc func keyboardDidShow(notification: Notification) {
+//        let info: NSDictionary = notification.userInfo! as NSDictionary
+//        let keyboardSize = (info[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+//        let keyboardY = self.view.frame.height - keyboardSize.height
+//
+//        let editingTextViewY: CGFloat! = self.notesTextview?.frame.origin.y
+//
+//        //Checking if the textView is really hidden behind the keyboard
+//        if self.view.frame.origin.y >= 0 {
+//            if editingTextViewY > keyboardY - 200 {
+//                UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+//                    self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y - (editingTextViewY! - (keyboardY - 200)), width: self.view.bounds.width, height: self.view.bounds.height)
+//                }, completion: nil)
+//            }
+//        }
+//    }
+//
+//    @objc func keyboardWillHide(notification: Notification) {
+//        UIView.animate(withDuration: 0.25, delay: 0.0, options: UIView.AnimationOptions.curveEaseIn, animations: {
+//            self.view.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+//        }, completion: nil)
+//    }
    
     func addDatePicker() {
         datePicker = UIDatePicker()
@@ -150,11 +151,7 @@ class AddEditItemViewController: UIViewController {
 
 extension AddEditItemViewController: UITextFieldDelegate, UITextViewDelegate {
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        //odometerTextfield.resignFirstResponder()
-        return true
-    }
-    
+    // TextField Delegates
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
         let oldText = textField.text!
@@ -164,20 +161,10 @@ extension AddEditItemViewController: UITextFieldDelegate, UITextViewDelegate {
         return true
     }
     
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        
-        let oldText = textView.text!
-        let stringRange = Range(range, in: oldText)!
-        let newText = oldText.replacingCharacters(in: stringRange, with: text)
-        doneBarButton.isEnabled = !newText.isEmpty
-        return true
-    }
-    
+    // TextView Delegates
     func textViewDidBeginEditing(_ textView: UITextView) {
         notesTextview = textView
     }
     
-    
-
 }
 
